46de8fb03bd1ad74ba59d467ff98355dad43e92c
Daniel Cassidy <mail@danielcassidy.me.uk>
2022-01-24 02:40:09 +0000

    feat(allasync): add allAsync function

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
 export type UnwrapPromiseLike<T> = T extends PromiseLike<infer U> ? U : T;
+
+type AllAsync<T extends Iterable<unknown>> = T extends readonly unknown[]
+    ? Promise<{[K in keyof T]: UnwrapPromiseLike<T[K]>}>
+    : T extends Iterable<infer U | PromiseLike<infer U>>
+    ? Promise<U[]>
+    : never;
+
+export function allAsync<T extends Iterable<unknown>>(values: T): AllAsync<T> {
+    return Promise.all(values) as unknown as AllAsync<T>;
+}
