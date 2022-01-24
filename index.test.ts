810c92e543defa55c03baf51cd843a5dc0cb5f3d
Daniel Cassidy <mail@danielcassidy.me.uk>
2022-01-24 02:56:59 +0000

    test(allasync): add test for allAsync

--- index.test.ts
+++ index.test.ts
@@ -1,3 +1,13 @@
 import test from "ava";
+import {allAsync} from "./index";
 
-test("placeholder test", t => t.true(true));
+test("allAsync", async t => {
+    const a = Promise.resolve(1 as const);
+    const b = Promise.resolve(2 as const);
+    const c = Promise.resolve(3 as const);
+    return allAsync([a, b, c] as const).then(([a, b, c]: readonly [1, 2, 3]) => {
+        t.is(a, 1);
+        t.is(b, 2);
+        t.is(c, 3);
+    });
+});
