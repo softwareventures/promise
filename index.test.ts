30563c9d7264d4b34b93af61c7797dfc79dd7ff4
Daniel Cassidy <mail@danielcassidy.me.uk>
2022-01-24 03:29:05 +0000

    test(allasync): add test for allAsync using Iterable

--- index.test.ts
+++ index.test.ts
@@ -5,9 +5,17 @@ test("allAsync", async t => {
     const a = Promise.resolve(1 as const);
     const b = Promise.resolve(2 as const);
     const c = Promise.resolve(3 as const);
-    return allAsync([a, b, c] as const).then(([a, b, c]: readonly [1, 2, 3]) => {
+    await allAsync([a, b, c] as const).then(([a, b, c]: readonly [1, 2, 3]) => {
         t.is(a, 1);
         t.is(b, 2);
         t.is(c, 3);
     });
+
+    function* generate(): Iterable<Promise<number>> {
+        yield Promise.resolve(1);
+        yield Promise.resolve(2);
+        yield Promise.resolve(3);
+    }
+
+    await allAsync(generate()).then(result => t.deepEqual(result, [1, 2, 3]));
 });
